069229a04cc8b07a3c540ae4746a3a6cc060251b
TheBlackSheep3 <40034835+TheBlackSheep3@users.noreply.github.com>
2020-05-29 17:14:23 +0200

    remove useless poll constructor
    * poll.js

--- poll.js
+++ poll.js
@@ -1,9 +1,4 @@
 class Poll {
-    constructor(id, title) {
-        this.id = id;
-        this.title = title;
-        this.description = "";
-    }
     constructor(id, title, description) {
         this.id = id;
         this.title = title;
