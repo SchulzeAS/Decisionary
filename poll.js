f32a026e4c24cc1d58efc4bf0b2ae8de7bb47275
TheBlackSheep3 <40034835+TheBlackSheep3@users.noreply.github.com>
2020-05-29 14:42:04 +0200

    create data structure
    + poll.js

---
+++ poll.js
@@ -0,0 +1,12 @@
+class Poll {
+    constructor(id, title) {
+        this.id = id;
+        this.title = title;
+        this.description = "";
+    }
+    constructor(id, title, description) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+    }
+}
